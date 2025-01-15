e1e4c2dea46190e238e32fe27bcd2da140873c5f
thanhnc-0773 <nguyen.chi.thanh@sun-asterisk.com>
2025-01-15 11:09:28 +0700

    upgrade-v21.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-facebook',
-      version='1.20.2',
+      version='1.21.0',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
@@ -13,7 +13,7 @@ setup(name='tap-facebook',
           'attrs==17.3.0',
           'backoff==2.2.1',
           'pendulum==1.2.0',
-          'facebook_business==19.0.2',
+          'facebook_business==21.0.5',
           'requests==2.20.0',
           'singer-python==6.0.0',
       ],
